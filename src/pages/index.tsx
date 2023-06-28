4fa5e935b28f9c8124693cd454509ed28314c520
Chad <chadkoslovsky@gmail.com>
2023-06-27 19:50:51 -0600

    added score/timer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,28 +7,44 @@ import { ToastContainer, toast } from "react-toastify";
 
 import "react-toastify/dist/ReactToastify.css";
 
-function checkGuess(collection: string, guess: string) {
-  // Replace this with your actual logic to check the user's guess
-  // Return value should be a boolean indicating whether the guess was correct
-  return collection === guess;
-}
-
 export default function Home() {
   const [nftData, setNftData] =
     useState<RouterOutputs["nft"]["getRandomNFT"]>();
-
-  const [refetchData, setRefetchData] = useState(false);
-
-  const [guess, setGuess] = useState(null);
-  const [result, setResult] = useState(null);
+  const [answers, setAnswers] = useState({
+    correct: 0,
+    incorrect: 0,
+  });
+  const [countdown, setCountdown] = useState(5);
 
   const nft = api.nft.getRandomNFT.useMutation();
+
+  useEffect(() => {
+    let countdownTimer: NodeJS.Timeout;
+
+    if (countdown > 0) {
+      countdownTimer = setTimeout(() => {
+        setCountdown((prevCountdown) => prevCountdown - 1);
+      }, 1000);
+    } else {
+      // Timeout expired, request a new NFT
+      requestNFT();
+    }
+
+    return () => {
+      clearTimeout(countdownTimer);
+    };
+  }, [countdown]);
 
   if (nft.isIdle && !nftData) {
     nft
       .mutateAsync()
       .then((data) => {
         setNftData(data);
+
+        setTimeout(() => {
+          // Only 5 seconds to guess
+          setCountdown(5);
+        }, 5000);
       })
       .catch((err) => {
         console.log(err);
@@ -36,29 +52,27 @@ export default function Home() {
   }
 
   const handleGuess = (collection: string) => {
-    // setGuess(collection);
     if (!nftData) return;
-    if (checkGuess(nftData?.contract, collection)) {
+
+    if (nftData.contract === collection) {
       toast.success("Correct!");
+      setAnswers((prev) => ({ ...prev, correct: prev.correct + 1 }));
     } else {
-      //get new contract
       toast.error("Nope!");
+      setAnswers((prev) => ({ ...prev, incorrect: prev.incorrect + 1 }));
     }
 
     setTimeout(() => {
       requestNFT();
     }, 500);
-
-    // setResult(isCorrect ? "Correct!" : "Sorry, try again");
   };
 
   function requestNFT() {
-    // setRefetchData(false);
-
     nft
       .mutateAsync()
       .then((data) => {
         setNftData(data);
+        setCountdown(5);
       })
       .catch((err) => {
         console.log(err);
@@ -76,16 +90,15 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e021d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Guess the{" "}
-            <span className="text-[hsl(280,100%,70%)]">NFT Collection</span>
+            Guess the <span className="text-[hsl(280,100%,70%)]">NFT</span>
           </h1>
 
           <img alt="NFT" src={nftData.image} />
 
-          <div className="flex justify-center gap-4">
+          <div className="flex justify-center gap-4 text-white">
             <button
               onClick={() =>
                 handleGuess("0xed5af388653567af2f388e6224dc7c4b3241c544")
@@ -102,12 +115,33 @@ export default function Home() {
             </button>
           </div>
 
-          {guess && (
-            <div>
-              <p>You guessed: {guess}</p>
-              <p>{result}</p>
+          <div className="mt-4 text-3xl text-white">
+            Time Remaining: {countdown} seconds
+          </div>
+        </div>
+
+        <div className="mt-5 flex flex-col items-center justify-center gap-4">
+          <h2 className=" text-2xl font-extrabold tracking-tight text-white sm:text-[2.5rem]">
+            Score
+          </h2>
+          <div className=" my-8 flex gap-8">
+            <div className="mx-10 flex flex-col items-center justify-center gap-2">
+              <span className="text-4xl font-extrabold tracking-tight text-white sm:text-[4rem]">
+                {answers.correct}
+              </span>
+              <span className="text-xl font-extrabold tracking-tight text-white sm:text-[1.5rem]">
+                Correct
+              </span>
             </div>
-          )}
+            <div className="mx-10 flex flex-col items-center justify-center gap-2">
+              <span className="text-4xl font-extrabold tracking-tight text-white sm:text-[4rem]">
+                {answers.incorrect}
+              </span>
+              <span className="text-xl font-extrabold tracking-tight text-white sm:text-[1.5rem]">
+                Incorrect
+              </span>
+            </div>
+          </div>
         </div>
       </main>
     </>
